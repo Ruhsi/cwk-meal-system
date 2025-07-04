281cd1e3d06582bf4974ccbf7184f3c42d9b6a9d
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2025-07-04 08:46:44 +0200

    Add cards for CWK2025

--- src/app/core/shared/data.service.ts
+++ src/app/core/shared/data.service.ts
@@ -24,46 +24,27 @@ export class DataService {
     let mealCards = new Array<MealCard>();
 
     mealCards = mealCards.concat(
-      this.createRedMealCard(97, 100),
-      this.createRedMealCard(276, 300),
-      this.createRedMealCard(301, 400),
       this.createRedMealCard(401, 500),
-      this.createRedMealCard(501, 600),
-      this.createRedMealCard(601, 700),
       this.createRedMealCard(701, 800),
       this.createRedMealCard(801, 900),
       this.createRedMealCard(901, 1000)
     );
 
     mealCards = mealCards.concat(
-      this.createYellowMealCard(12, 100),
-      this.createYellowMealCard(101, 200),
-      this.createYellowMealCard(366, 400),
-      this.createYellowMealCard(643, 700),
-      this.createYellowMealCard(701, 800),
-      this.createYellowMealCard(801, 900),
-      this.createYellowMealCard(901, 1000)
-    );
-
-    mealCards = mealCards.concat(
-      this.createGreenMealCard(255, 300),
       this.createGreenMealCard(385, 400),
       this.createGreenMealCard(413, 500),
       this.createGreenMealCard(501, 600),
-      this.createGreenMealCard(650, 700),
-      this.createGreenMealCard(780, 800)
+      this.createGreenMealCard(650, 700)
     );
 
     mealCards = mealCards.concat(
-      this.createBlueMealCard(149, 200),
+      this.createBlueMealCard(165, 200),
       this.createBlueMealCard(201, 300),
-      this.createBlueMealCard(301, 400),
       this.createBlueMealCard(401, 500),
       this.createBlueMealCard(501, 600),
       this.createBlueMealCard(601, 700),
       this.createBlueMealCard(701, 800),
-      this.createBlueMealCard(801, 900),
-      this.createBlueMealCard(901, 1000)
+      this.createBlueMealCard(974, 1000)
     );
 
     this.setData(mealCards);
@@ -79,7 +60,8 @@ export class DataService {
       }
       return mealCard;
     });
-    this.setData(mealCards.sort((a, b) => a.name - b.name));
+    let data = mealCards.sort((a, b) => a.name - b.name);
+    this.setData(data);
   }
 
   private createBlueMealCard(from: number, to: number): Array<MealCard> {
